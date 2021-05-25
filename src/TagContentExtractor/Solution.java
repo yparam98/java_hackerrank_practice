f21134a2ddd5ea237c6296b506dd71f7b8c25a74
yparam98 <yathavanparamesh1998@gmail.com>
2021-05-24 20:09:53 -0400

    finished challenge

--- src/TagContentExtractor/Solution.java
+++ src/TagContentExtractor/Solution.java
@@ -3,20 +3,19 @@ package TagContentExtractor;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
 import java.util.stream.Stream;
 
 public class Solution{
     public static void main(String[] args)
     {
         try {
-            String[] test_cases = get_cases("src/TagContentExtractor/input02.txt");
-            Stream.of(test_cases).forEach(line -> System.out.println(get_tag(line)));
+            String[] test_cases = get_cases("src/TagContentExtractor/input01.txt");
+            Stream.of(test_cases).forEach(line -> Stream.of(get_tag(line)).forEach(System.out::println));
         } catch (IOException ioException) {
             System.out.println("Error reading data: " + ioException.getLocalizedMessage());
         } catch (Exception exception) {
-            System.out.println("Error processing data: " + exception.getLocalizedMessage());
+            System.out.println("Error processing data: " + exception.toString());
         }
     }
 
@@ -37,24 +36,31 @@ public class Solution{
         return test_cases;
     }
 
-    private static String get_tag(String line)
+    private static String[] get_tag(String line)
     {
-        String result = "";
+        String[] split_strings = line.split("<|>");
+        ArrayList<String> results = new ArrayList<>();
 
-        if (!line.isEmpty()) {
-            Pattern tag_matcher = Pattern.compile("<(..*)>(.*)</(.*)(?:\\1)>");
-            Matcher matching_tags = tag_matcher.matcher(line);
-
-            while (matching_tags.find()) {
-                result = matching_tags.group(2);
-                matching_tags = tag_matcher.matcher(result);
+        for (int index = 0; index < split_strings.length; index++)
+        {
+            if (!split_strings[index].isEmpty()
+                    && index+2 < split_strings.length
+                    && !split_strings[index+1].isEmpty()
+                    && !split_strings[index+2].isEmpty()
+            )
+            {
+                if (split_strings[index+2].substring(1).equals(split_strings[index]))
+                {
+                    results.add(split_strings[index+1]);
+                }
             }
         }
 
-        if ((result.equals(line) || result.isEmpty()) || result.contains("<") || (result.length() > Math.pow(10, 4))) {
-            return "Nope";
-        } else {
-            return result;
+        if (results.isEmpty())
+        {
+            results.add("None");
         }
+
+        return results.toArray(String[]::new);
     }
 }
